439ebe3628166917009386d511e16e68061f7678
biwano <biwano@gmail.com>
2024-11-14 17:07:20 +0100

    UI

--- src/components/Copiable.vue
+++ src/components/Copiable.vue
@@ -10,26 +10,7 @@ function onCopy() {
 }
 
 </script>
-<style scoped>
-.item {
-  display: flex;
-  gap: 1rem;
-}
-.item>div {
-  flex: 1 1 0;
-  padding: var(--item-padding);
-  display: flex;
-  align-items: center;
-}
-.item>div:first-child {
-  font-weight: bolder;
 
-  background-color: var(--color-background-soft);
-}
-.item>div:nth-child(2) {
-  flex: 2 1 0;
-}
-</style>
 <template>
-    <div class="item"><div>{{label}}</div><div v-on:click="onCopy">{{ uiValueLabel }}</div></div>
+    <div class="row"><div>{{label}}</div><div v-on:click="onCopy">{{ uiValueLabel }}</div></div>
 </template>
--- src/components/ViewTransaction.vue
+++ src/components/ViewTransaction.vue
@@ -5,34 +5,17 @@ import Copiable from "./Copiable.vue";
 
 </script>
 <style scoped>
-.item {
-  display: flex;
-  gap: 1rem;
-}
-.item>div {
-  flex: 1 1 0;
-  padding: var(--padding);
-  display: flex;
-  align-items: center;
-}
-.item>div:first-child {
-  font-weight: bolder;
 
-  background-color: var(--color-background-soft);
-}
-.item>div:nth-child(2) {
-  flex: 2 1 0;
-}
 </style>
 <template>
   <div class="card">
     <h1>Fetch transaction</h1>
     <form class="column">
-      <div class="item">
+      <div class="row">
         <div>Transaction refId</div>
         <div><input /></div>
       </div>
-      <div>
+      <div class="row">
         <button>Submit</button>
       </div>
     </form>
